a73a49602b0ee45c4d1acc7e706dbc8f24920be2
yugykim <yugyeongk92@gmail.com>
2023-01-16 10:02:18 -0700

    Created Gallery Component

--- capstone-project-frontend/src/components/header.tsx
+++ capstone-project-frontend/src/components/header.tsx
@@ -5,12 +5,18 @@ export const Header = () => {
 	return (
 		<header className='py-30 bg-red-300'>
 			<div className='w-full px-5 xl:px-0 max-w-screen-2xl mx-auto  items-center flex justify-between'>
-				<span className='text-xs'>
-					<FontAwesomeIcon
-						icon={faUser}
-						className='text-xl'
-					/>
-				</span>
+        <nav className=''>
+          <div className='text-xs'>
+            <FontAwesomeIcon
+              icon={faUser}
+              className='text-xl'
+            />
+          </div>
+          <div>about</div>
+          <div>our work</div>
+          <div>contact us</div>
+          <div>sign in</div>
+        </nav>
 			</div>
 		</header>
 	);

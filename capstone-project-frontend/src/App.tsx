035df3d3e4e0d361ed5fdb65552e772c4c6c1e3f
yugykim <yugyeongk92@gmail.com>
2023-01-18 08:53:35 -0700

    Finished Configured TailwindCSS

--- capstone-project-frontend/src/App.tsx
+++ capstone-project-frontend/src/App.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
@@ -7,19 +9,37 @@ import { SignUp } from './pages/create-account';
 import { Reservation } from './pages/reservation';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <>
-          <Route key={1} path="/" element={<Home />} />
-          <Route key={2} path="/logIn" element={<LogIn />} />
-          <Route key={3} path="/create-account" element={<SignUp />} />
-          <Route key={4} path="/reservation" element={<Reservation />} />
-        </>
-      </Routes>
-    </Router>
-  );
+	return (
+		<>
+			<Router>
+				<Header />
+				<Routes>
+					<>
+						<Route
+							key={1}
+							path='/'
+							element={<Home />}
+						/>
+						<Route
+							key={2}
+							path='/logIn'
+							element={<LogIn />}
+						/>
+						<Route
+							key={3}
+							path='/create-account'
+							element={<SignUp />}
+						/>
+						<Route
+							key={4}
+							path='/reservation'
+							element={<Reservation />}
+						/>
+					</>
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;

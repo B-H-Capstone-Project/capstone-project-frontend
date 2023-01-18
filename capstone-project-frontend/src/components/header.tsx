5370d3d0b8818629859592f1bb6ad9a9e69168d0
yugykim <yugyeongk92@gmail.com>
2023-01-18 09:16:55 -0700

    React DOM error

--- capstone-project-frontend/src/components/header.tsx
+++ capstone-project-frontend/src/components/header.tsx
@@ -1,16 +1,22 @@
+import React from 'react';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Header = () => {
 	return (
-		<header className='py-50 bg-red-500'>
-        <nav>
-          <div className='text-xs'><FontAwesomeIcon icon={faUser} className='text-xl' /></div>
-          <div>about</div>
-          <div>our work</div>
-          <div>contact us</div>
-          <div>sign in</div>
-        </nav>
+		<header className='py-'>
+			<nav className='w-full px-5 xl:px-0 max-w-screen-2xl mx-auto items-center flex justify-between'>
+				<div className='px-4 bg-red-300'>about</div>
+				<div className='px-4 bg-red-300'>our work</div>
+				<div className='px-4 bg-red-300'>contact us</div>
+				<div className='px-4 bg-red-300'>sign in</div>
+				<div className='text'>
+					<FontAwesomeIcon
+						icon={faUser}
+						className='text-xl'
+					/>
+				</div>
+			</nav>
 		</header>
 	);
 };

770ab38ab03a51b129b8c0ef2b5c49ab9890b30d
yugykim <yugyeongk92@gmail.com>
2023-01-19 15:59:38 -0700

    Created 404 pages

--- capstone-project-frontend/src/components/header.tsx
+++ capstone-project-frontend/src/components/header.tsx
@@ -6,19 +6,21 @@ import { HashLink } from 'react-router-hash-link'
 
 export const Header = () => {
 	return (
-		<header className='bg-red-300 py-4'>
+		<header className='py-4'>
 			<nav className='w-full px-5 xl:px-0 max-w-screen-2xl mx-auto items-center flex justify-between'>
 				<div>
+          <Link to={"/"}>
 					<FontAwesomeIcon
 						icon={faUser}
 						className='text-xl'
 					/>
+          </Link>
 				</div>
         <div className='flex justify-between'>
           <div className='py-2 px-14'><HashLink smooth to={"#about"}>about</HashLink></div>
           <div className='py-2 px-14'><HashLink smooth to={"#our-work"}>our work</HashLink></div>
           <div className='py-2 px-14'><HashLink smooth to={"#contact-us"}>contact us</HashLink></div>
-          <button className=' bg-black hover:bg-blue-700 py-2 px-4  text-white font-bold rounded'><Link to={"/signIn"}>SIGN IN</Link></button>
+          <button className=' bg-black hover:bg-blue-700 py-4 px-10  text-white font-bold rounded'><Link to={"/signIn"}>SIGN IN</Link></button>
         </div>
 			</nav>
 		</header>

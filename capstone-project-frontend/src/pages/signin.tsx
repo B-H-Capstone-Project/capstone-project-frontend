640e0f180749173880de56660c47d44155657a30
abdulosmann <abdulrahman.osman@edu.sait.ca>
2023-01-23 10:49:09 -0700

    testTable

--- capstone-project-frontend/src/pages/signin.tsx
+++ capstone-project-frontend/src/pages/signin.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import * as React from "react";
 export const SignIn = () => {
   return (
     <>
@@ -28,6 +29,7 @@ export const SignIn = () => {
                     type="text"
                     className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Email address"
+
                   />
                 </div>
                 {/* Password Input Div */}

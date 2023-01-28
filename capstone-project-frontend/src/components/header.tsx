390def43034a70246d061ca16ac3bdf3c879fc2b
yugykim <yugyeongk92@gmail.com>
2023-01-28 14:54:45 -0700

    Responsive main background

--- capstone-project-frontend/src/components/header.tsx
+++ capstone-project-frontend/src/components/header.tsx
@@ -4,8 +4,8 @@ import { HashLink } from 'react-router-hash-link'
 
 export const Header = () => {
 	return (
-		<header className='py-4 px-5'>
-			<nav className='w-full xl:px-0 max-w-screen-2xl items-center flex justify-between'>
+		<header>
+			<nav className='flex justify-between px-10 sm:px-4 py-2.5'>
 				<div className='text-3xl font-bold ml-8'>
           <Link to={"/"}>
             B&H

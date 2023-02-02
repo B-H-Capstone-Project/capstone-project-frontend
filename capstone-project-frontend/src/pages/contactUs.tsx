aa35961f92d382a18f58a015d5e6870125c9ed64
abdulosmann <abdulrahman.osman@edu.sait.ca>
2023-02-02 12:05:04 -0700

    contact

--- capstone-project-frontend/src/pages/contactUs.tsx
+++ capstone-project-frontend/src/pages/contactUs.tsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export const ContactUs = () => {
-  return <h1>contact us</h1>
+  return (
+    <div>
+      <img className="inline" src="..." alt="..."></img>
+    <div className='flex justify-center'>
+          <h1>CONTACT:</h1>
+          </div>
+          <div className='py-2 px-14 flex justify-center'><h2>403-630-1277</h2></div>
+          <div className='py-2 px-14 flex justify-center'><h2><a href="mailto:info@bossandhoss.com">info@bossandhoss.com</a></h2></div>
+          <div className='py-2 px-14 flex justify-center'><h2><Link to={"/Instagram"}>Boss and Hoss Instagram</Link></h2></div>
+          <div className='flex justify-center'><h1>OFFICE:</h1></div>
+          <div className='py-2 px-14 flex justify-center'><h3>3420 Temple Road NE</h3></div>
+          <div className='py-2 px-14 flex justify-center'><h3>Calgary, AB T1Y 3A9, Canada</h3></div>
+        </div>
+  )
 }

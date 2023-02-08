3c7b1fd9aafb879abdabc9451b062b6e3e0e4c74
abdulosmann <abdulrahman.osman@edu.sait.ca>
2023-02-08 08:34:01 -0700

    :;

--- capstone-project-frontend/src/routers/logged-out-router.tsx
+++ capstone-project-frontend/src/routers/logged-out-router.tsx
@@ -8,18 +8,14 @@ import {
 	Navigate,
 } from 'react-router-dom';
 import { NotFound } from '../pages/404';
-<<<<<<< HEAD
 import { SignIn } from '../pages/signin';
 import { Header } from '../components/header';
 import { Home } from '../pages/home';
 import { SignUp } from '../pages/signup';
-=======
-import { Header } from '../components/header';
-import { Home } from '../pages/home';
-import { SignIn } from '../pages/signin';
-import { SignUp } from '../pages/signup';
+import CheckYourEmail from '../components/checkYourEmail';
+import VerifyEmail from '../components/verifyEmail';
+import { ContactUs } from '../pages/contactUs';
 import { OurWork } from '../pages/our-work';
->>>>>>> f6cfa20cb12b5826333d984e33361d7a0848d09b
 
 export const LoggedOutRouter = () => {
 	return (

15fe381bfbddcd7bc0efe31a17ece44d1fa5cd1d
Den9077 <95991934+Den9077@users.noreply.github.com>
2023-02-10 09:04:14 -0700

    update ourwork page

--- capstone-project-frontend/src/pages/our-work.tsx
+++ capstone-project-frontend/src/pages/our-work.tsx
@@ -1,3 +1,41 @@
+import React from 'react';
+import { imgData } from './galleryImg';
+import { MdChevronLeft, MdChevronRight} from 'react-icons/md';
+
 export const OurWork = () => {
-  return<h1>Gallery</h1>
-}
+
+  // const slideLeft = () => {
+  //   let slider = document.getElementById('slider')
+  //   slider.scrollLeft = slider.scrollLeft - 500
+  // }
+
+  // const slideRight = () => {
+    
+  //   let slider = document.getElementById('slider')
+  //   slider.scrollLeft = slider.scrollLeft + 500
+  // }
+  
+  
+
+  return (
+
+
+    <>
+      <div className='relative flex items-center'>
+        <MdChevronLeft  size={40}/>
+        <div id='slider' className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth' >
+          {imgData.map((item) => (
+            <img
+            className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 scroll-smooth' 
+            src={item.img} 
+            alt='/' />
+          )
+          )}         
+        </div>
+        <MdChevronRight size={40}/>
+
+      </div>
+    </>
+  );
+};
+
--- capstone-project-frontend/src/routers/logged-out-router.tsx
+++ capstone-project-frontend/src/routers/logged-out-router.tsx
@@ -8,18 +8,12 @@ import {
 	Navigate,
 } from 'react-router-dom';
 import { NotFound } from '../pages/404';
-<<<<<<< HEAD
-import { SignIn } from '../pages/signin';
-import { Header } from '../components/header';
-import { Home } from '../pages/home';
-import { SignUp } from '../pages/signup';
-=======
+
 import { Header } from '../components/header';
 import { Home } from '../pages/home';
 import { SignIn } from '../pages/signin';
 import { SignUp } from '../pages/signup';
 import { OurWork } from '../pages/our-work';
->>>>>>> f6cfa20cb12b5826333d984e33361d7a0848d09b
 
 export const LoggedOutRouter = () => {
 	return (

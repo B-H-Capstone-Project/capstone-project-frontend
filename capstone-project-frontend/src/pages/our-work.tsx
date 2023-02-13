9ea69e206fa1d7c332863e5e5fc7439eecd95f27
Den9077 <95991934+Den9077@users.noreply.github.com>
2023-02-13 08:49:44 -0700

    Create slide show on Our-work page

--- capstone-project-frontend/src/pages/our-work.tsx
+++ capstone-project-frontend/src/pages/our-work.tsx
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 import { imgData } from './galleryImg';
-import { MdChevronLeft, MdChevronRight} from 'react-icons/md';
+import { MdChevronRight, MdChevronLeft } from "react-icons/md";
+
 
 export const OurWork = () => {
 
-  // const slideLeft = () => {
-  //   let slider = document.getElementById('slider')
-  //   slider.scrollLeft = slider.scrollLeft - 500
-  // }
+  const slideLeft = () => {
+    const slider = document.getElementById('slider')
+    slider!.scrollLeft = slider!.scrollLeft - 440
+  }
 
-  // const slideRight = () => {
-    
-  //   let slider = document.getElementById('slider')
-  //   slider.scrollLeft = slider.scrollLeft + 500
-  // }
-  
-  
+  const slideRight = () => {
+
+    let slider = document.getElementById('slider')
+    slider!.scrollLeft = slider!.scrollLeft + 440
+  }
+
+
 
   return (
-
-
     <>
-      <div className='relative flex items-center'>
-        <MdChevronLeft  size={40}/>
-        <div id='slider' className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth' >
-          {imgData.map((item) => (
-            <img
-            className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 scroll-smooth' 
-            src={item.img} 
-            alt='/' />
-          )
-          )}         
-        </div>
-        <MdChevronRight size={40}/>
 
+      <div className="p-4 mx-16 space-y-4">
+        {/* <img className="object-scale-down "src="https://www.linkpicture.com/q/1-1_26.jpg"></img> */}
+        <div className='relative flex items-center'>
+        <button onClick={slideLeft}><MdChevronLeft size={50} /></button>
+        <div>
+          <div id='slider' className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide ' >
+            {imgData.map((item) => (
+              <img onClick={(slideLeft)}
+                className='w-[220px] inline-block p-2 cursor-pointer hover:scale-115 ease-in-out duration-300 scroll-smooth'
+                src={item.img}
+                alt='/' />
+            )
+            )}
+          </div>
+        </div>
+        <button onClick={slideRight}><MdChevronRight size={50} /></button>
       </div>
+      </div>
+
+
     </>
   );
 };

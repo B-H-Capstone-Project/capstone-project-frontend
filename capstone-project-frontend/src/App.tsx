b5edf10b508da755da6b15e77c18bd36710200a9
yugykim <yugyeongk92@gmail.com>
2023-02-13 10:47:13 -0700

    backend url address

--- capstone-project-frontend/src/App.tsx
+++ capstone-project-frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { LoggedInRouter } from "./routers/logged-in-router";
-import { LoggedOutRouter } from "./routers/logged-out-router";
+/** @format */
+
+import React from 'react';
+import { LoggedInRouter } from './routers/logged-in-router';
+import { LoggedOutRouter } from './routers/logged-out-router';
+import { LOCAL_STORAGE_TOKEN } from  '../src/constant'
 
 function App() {
-	return <LoggedOutRouter />;
+	const token = localStorage.getItem('token');
+  const loggedIn = Boolean(token);
+  console.log(loggedIn);
+	return loggedIn?  <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;

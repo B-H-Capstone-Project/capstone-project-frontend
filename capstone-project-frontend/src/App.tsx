cd2323168bb33794addf802f53c4e36f23c0a9f3
JenHLee <hj.jennie.lee@gmail.com>
2023-02-21 00:12:45 -0700

    add reservation, reservation form, edit admin

--- capstone-project-frontend/src/App.tsx
+++ capstone-project-frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
+import { truncate } from 'fs';
 
 
 function App() {
-	const token = localStorage.getItem('token');
-  //const isLoggedIn = Boolean(token);
+  // console.log('----------------app---------------');
+  // console.log(localStorage.token);
+	// return (localStorage.token === undefined ? 
+  // <LoggedOutRouter />  :  <LoggedInRouter /> 
+  //  );
+  // return ( <LoggedOutRouter />);
+
   const isLoggedIn = true;
 	return (isLoggedIn? <LoggedInRouter /> : <LoggedOutRouter />);
 }
+
 
 export default App;
  

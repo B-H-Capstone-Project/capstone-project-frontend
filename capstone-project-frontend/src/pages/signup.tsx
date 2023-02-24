46f37d148b6c00827af2a80a4d63fa60dafb0e20
abdulosmann <abdulrahman.osman@edu.sait.ca>
2023-02-23 17:20:59 -0700

    signup good

--- capstone-project-frontend/src/pages/signup.tsx
+++ capstone-project-frontend/src/pages/signup.tsx
@@ -78,9 +78,9 @@ export const SignUp = () => {
                 </Link>
               </p>
               <form onSubmit={handleSubmit(onSubmit)}>
-                {/* FirstName & LastName */}
-                <div className="w-1/2 flex flex-row gap-4">
-                  <div className="flex flex-col">
+                {/* FirstName & LastName */}   
+                <div className="flex flex-row gap-4">           
+                <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       First Name *
                     </label>
@@ -88,11 +88,11 @@ export const SignUp = () => {
                       type="text"
                       id="first_name"
                       {...register('first_name')}
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="John"
                     />
                   </div>
-                  <div className="flex flex-col">
+                  <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       Last Name *
                     </label>
@@ -100,11 +100,11 @@ export const SignUp = () => {
                       type="text"
                       id="last_name"
                       {...register('last_name')}
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Doe"
                     />
                   </div>
-                </div>
+                  </div>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                     Phone Number *
@@ -181,8 +181,8 @@ export const SignUp = () => {
                     />
                 </div>
                 {/* Postal Code & City */}
-                <div className="w-1/2 flex flex-row gap-4">
-                  <div className="flex flex-col">
+                <div className="flex flex-row gap-4">           
+                <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       Postal Code *
                     </label>
@@ -190,11 +190,11 @@ export const SignUp = () => {
                       type="text"
                       id="postal_code"
                       {...register('postal_code')}
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
 
-                  <div className="flex flex-col">
+                  <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       City *
                     </label>
@@ -202,14 +202,14 @@ export const SignUp = () => {
                       type="text"
                       id="city"
                       {...register('city')}
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
                 </div>
 
                 {/* Province & Country */}
-                <div className="w-1/2 flex flex-row gap-4">
-                  <div className="flex flex-col">
+                <div className="flex flex-row gap-4">           
+                <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       Province *
                     </label>
@@ -217,11 +217,11 @@ export const SignUp = () => {
                       type="text"
                       {...register('province')}
                       id="province"
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
 
-                  <div className="flex flex-col">
+                  <div className="md:w-1/2">
                     <label className="block mb-2 text-sm font-medium text-black-100 dark:text-black">
                       Country *
                     </label>
@@ -229,7 +229,7 @@ export const SignUp = () => {
                       type="text"
                       id="country"
                       {...register('country')}
-                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-white-50 border border-white-300 text-black-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 block w-full bg-white-700 border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       />
                   </div>
                 </div>
@@ -261,25 +261,25 @@ export const SignUp = () => {
                     Create account
                   </button>
                 </div>
-                <p>
-                  ---------------------------- or ----------------------------
-                </p>
-                <div className="flex justify-center gap-4 mt-2 mb-2">
-                  <div className='flex justify-start'>
-                  <button
-                    type="submit"
-                    className="  bg-lime-500 active:bg-lime-500 hover:bg-lime-500 focus:bg-lime-500 text-white font-bold py-2 px-4 rounded">
-                    Google
-                  </button>
-                  </div>
-                  <div className='flex justify-end'>
-                  <button
-                    type="submit"
-                    className="  bg-lime-500 active:bg-lime-500 hover:bg-lime-500 focus:bg-lime-500 text-white font-bold py-2 px-4 rounded">
-                    Facebook
-                  </button>
-                  </div>
-                </div>
+                <div className="flex justify-between items-center mt-3">
+            <hr className="w-full"/>
+            <span className="p-2 text-gray-400 mb-1">OR</span>
+            <hr className="w-full"/>
+            </div>
+				<div className="flex items-center flex-row gap-4">           
+                  <div className="md:w-1/2">
+						<button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4] focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                          <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
+                            Sign in with Google
+                            </button>
+							  </div>
+				  <div className="md:w-1/2">
+						<button type="button" className="text-white bg-[#3b5998] hover:bg-[#3b5998] focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">
+                          <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"></path></svg>
+                            Sign in with Facebook
+                            </button>
+                            </div>
+                            </div>
                 <div className='flex justify-center mt-4'>
                 <label>
                   <Link to='guest'

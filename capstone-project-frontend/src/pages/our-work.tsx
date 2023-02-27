a347bf25924a52819b5c153596ef5053d4c95499
Den9077 <95991934+Den9077@users.noreply.github.com>
2023-02-27 09:54:58 -0700

    edit outwork page

--- capstone-project-frontend/src/pages/our-work.tsx
+++ capstone-project-frontend/src/pages/our-work.tsx
@@ -28,8 +28,7 @@ export const OurWork = () => {
 
   return (
     <>
-      <Header></Header>
-      <div className="p-8 mx-auto  space-y-4 ">
+      <div className="p-8 mx-auto h-screen w-screen space-y-4 ">
 
 
         {/* <img className="w-full h-[440px] object-cover" src="https://www.linkpicture.com/q/1-1_26.jpg"></img>  */}
@@ -37,7 +36,7 @@ export const OurWork = () => {
         <div className='relative flex items-center  '>
           <button onClick={slideLeft}><MdChevronLeft size={50} /></button>
 
-          <div id='slider' className='w-full h-full object-cover overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide ' > {/* Note: Scrollbar-hide does not apply... */}
+          <div id='slider' className=' w-full h-full object-cover overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide ' > {/* Note: Scrollbar-hide does not apply... */}
             {imgData.map((item, index) => (
               <img onClick={() => getImg(item.imgSrc)}
                 className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 '
@@ -49,11 +48,13 @@ export const OurWork = () => {
           <button onClick={slideRight}><MdChevronRight size={50} /></button>
         </div>
         <div 
+          className=' h-1/2 w-1/2 mx-auto inline-flex '
           id={model ? "model open" : "model"}>
-          <img src={tempimgSrc} className='' />
+          <img src={tempimgSrc} className=" " />
 
         </div>
       </div>
+  
 
 
 

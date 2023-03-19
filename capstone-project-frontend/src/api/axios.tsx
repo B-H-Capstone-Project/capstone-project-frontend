504784423d6ee36176bdb016fa42a272ce979fdb
yugykim <yugyeongk92@gmail.com>
2023-03-19 14:36:28 -0600

    finished mobile css

--- capstone-project-frontend/src/api/axios.tsx
+++ capstone-project-frontend/src/api/axios.tsx
@@ -3,6 +3,11 @@ import env from "ts-react-dotenv";
 
 // console.log(env.BACKEND_IP);
 export default axios.create({
+<<<<<<< HEAD
   // baseURL: `http://${env.BACKEND_IP}:8080`,
   baseURL: `http://localhost:8080`,
 });
+=======
+    baseURL: `http://${env.BACKEND_IP}:8080`
+});  
+>>>>>>> 5e65e79 (finished mobile css)

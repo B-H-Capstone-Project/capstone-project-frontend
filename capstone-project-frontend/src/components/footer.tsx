9928416f0d3bfd28ae5f87e0d4242f262cc468db
yugykim <yugyeongk92@gmail.com>
2023-04-08 20:12:08 -0600

    Fixed Footer

--- capstone-project-frontend/src/components/footer.tsx
+++ capstone-project-frontend/src/components/footer.tsx
@@ -1,6 +1,7 @@
 /** @format */
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GoogleIcon from '@mui/icons-material/Google';
+import React from 'react';
 
 
 export const Footer = () => {
